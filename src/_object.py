20fc900755ac150442a78da5b68ca02eb978191d
One Nose <idohaolam@gmail.com>
2023-03-01 14:08:53 +0200

    Added `Object.behaviors` and `Object.register()`

--- src/_object.py
+++ src/_object.py
@@ -1,2 +1,11 @@
+from _behavior import Behavior
+
+
 class Object:
-    pass
+    behaviors: list[Behavior]
+
+    def __init__(self) -> None:
+        self.behaviors = []
+
+    def register(self, behavior: Behavior) -> None:
+        self.behaviors.append(behavior)
